c74345a2eae8009b37daa77fcb24292e9b39b0f0
Wo1f27 <avstrelkov@yandex.ru>
2023-11-20 15:04:37 +0300

    Update README.md

--- autoru_catalog/settings.py
+++ autoru_catalog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-miez!q$h^hr&iwy+=98qdibunsj7^h)x%c%mjn5zr$ottivvcg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
